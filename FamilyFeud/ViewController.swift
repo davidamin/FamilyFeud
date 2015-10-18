db00dc264dab51c80c7696512354e3a129214613
David Amin <dma3fq@labmini00-27.local>
2015-10-18 14:26:51 -0400

    Create Labels

--- FamilyFeud/ViewController.swift
+++ FamilyFeud/ViewController.swift
@@ -8,16 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var nameText: UITextField!
+    
+    @IBOutlet weak var nameBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        nameText.delegate = self
+        nameBtn.addTarget(self, action: "setName:", forControlEvents: UIControlEvents.TouchUpInside)    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func setName(sender: UIButton){
+        nameLabel.text = "Hello " + nameText.text! + ", and welcome to Family Feud!"
     }
 
 
