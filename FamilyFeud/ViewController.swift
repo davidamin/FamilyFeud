7d7515e5897a835f024a70812994e3d31b771696
David Amin <dma3fq@labmini04.local>
2015-11-23 16:16:02 -0500

    Login works

--- FamilyFeud/ViewController.swift
+++ FamilyFeud/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController, UITextFieldDelegate, CLLocationManagerDe
 
     @IBOutlet weak var nameLabel: UILabel!
     
+    @IBOutlet weak var passText: UITextField!
+
     @IBOutlet weak var nameText: UITextField!
     
     @IBOutlet weak var nameBtn: UIButton!
@@ -30,6 +32,8 @@ class ViewController: UIViewController, UITextFieldDelegate, CLLocationManagerDe
     var username = NSManagedObject?()
     
     var locationManager:CLLocationManager?
+    
+    var logged_in : Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -192,8 +196,82 @@ class ViewController: UIViewController, UITextFieldDelegate, CLLocationManagerDe
     }
     
     @IBAction func setName(sender: UIButton){
+        
+        if(logged_in){
+            self.performSegueWithIdentifier("StartGameSegue", sender: nil)
+            return
+        }
+        let postEndpoint: String = "http://ec2-54-174-16-239.compute-1.amazonaws.com/add_user/"
+        guard let url = NSURL(string: postEndpoint) else {
+            print("Error: cannot create URL")
+            return
+        }
+        
+        let urlRequest = NSMutableURLRequest(URL: url)
+        urlRequest.HTTPMethod = "POST"
+        
+        let newPost: NSDictionary = ["username": "ghdfsughfhagvijek", "password": "password"]
+        do {
+            let postString = "username=" + nameText.text! + "&password=" + passText.text!
+            urlRequest.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding)
+            //let jsonPost = try NSJSONSerialization.dataWithJSONObject(newPost, options: [])
+            //urlRequest.HTTPBody = jsonPost
+        } catch {
+            print("Error: cannot create JSON from post")
+        }
+        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session = NSURLSession(configuration: config)
+        let task = session.dataTaskWithRequest(urlRequest, completionHandler: { (data, response, error) in
+            guard let responseData = data else {
+                print("Error: did not receive data")
+                return
+            }
+            guard error == nil else {
+                print("error calling GET")
+                print(error)
+                return
+            }
+            //self.nameLabel.text = String(responseData)
+            //parse the result as JSON, since that's what the API provides
+            let post: NSDictionary
+            do {
+                post = try NSJSONSerialization.JSONObjectWithData(responseData,
+                    options: []) as! NSDictionary
+            } catch  {
+                print("error trying to convert data to JSON")
+                print(String(responseData))
+                return
+            }
+            // now we have the post, let's just print it to prove we can access it
+            print("The post is: " + post.description)
+            
+            // the post object is a dictionary
+            // so we just access the title using the "title" key
+            // so check for a title and print it if we have one
+            if let valid = post["ok"] as? Bool{
+                if(valid){
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.nameLabel.text = "Logged In!"
+                    self.nameBtn.setTitle("Play!", forState: UIControlState.Normal)
+                }
+                self.logged_in = true
+                }else{
+                    dispatch_async(dispatch_get_main_queue()) {
+                        self.nameLabel.text = "Login failed."
+                    }
+                }
+                //self.performSegueWithIdentifier("StartGameSegue", sender: nil)
+            }
+        })
+        task.resume()
+        
+
+        
+        
+        
+        
         nameText.resignFirstResponder()
-        let managedObjectContext = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
+        /*let managedObjectContext = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
         
         //Let's check the database for a user with the name we just input
         let fetchRequest = NSFetchRequest(entityName: "User")
@@ -223,7 +301,7 @@ class ViewController: UIViewController, UITextFieldDelegate, CLLocationManagerDe
             } catch let error as NSError  {
                 print("Could not save \(error), \(error.userInfo)")
             }
-        }
+        }*/
         
         //nameLabel.text = "Hello " + nameText.text! + ", and welcome to Family Feud!"
     }
