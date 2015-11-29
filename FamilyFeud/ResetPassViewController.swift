7766f6c9e57cd63076cc3864d0952f267a0887aa
David Amin <dma3fq@labmini08.local>
2015-11-29 16:02:30 -0500

    Logic for web calls

--- FamilyFeud/ResetPassViewController.swift
+++ FamilyFeud/ResetPassViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ResetPassViewController: UIViewController {
     
+    @IBOutlet weak var errorLabel: UILabel!
+    
     @IBOutlet weak var nameText: UITextField!
     
     @IBOutlet weak var emailText: UITextField!
@@ -27,7 +29,70 @@ class ResetPassViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func sendEmail(sender: UIButton){
+        
+        let postEndpoint: String = "http://ec2-54-174-16-239.compute-1.amazonaws.com/send_email/"
+        guard let url = NSURL(string: postEndpoint) else {
+            print("Error: cannot create URL")
+            return
+        }
+        
+        let urlRequest = NSMutableURLRequest(URL: url)
+        urlRequest.HTTPMethod = "POST"
+        
+        do {
+            let postString = "username=" + nameText.text! + "&email=" + emailText.text!
+            urlRequest.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding)
+            //let jsonPost = try NSJSONSerialization.dataWithJSONObject(newPost, options: [])
+            //urlRequest.HTTPBody = jsonPost
+        } catch {
+            print("Error: cannot create JSON from post")
+        }
+        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session = NSURLSession(configuration: config)
+        let task = session.dataTaskWithRequest(urlRequest, completionHandler: { (data, response, error) in
+            guard let responseData = data else {
+                print("Error: did not receive data")
+                return
+            }
+            guard error == nil else {
+                print("error calling GET")
+                print(error)
+                return
+            }
+            //self.nameLabel.text = String(responseData)
+            //parse the result as JSON, since that's what the API provides
+            let post: NSDictionary
+            do {
+                post = try NSJSONSerialization.JSONObjectWithData(responseData,
+                    options: []) as! NSDictionary
+            } catch  {
+                print("error trying to convert data to JSON")
+                print(String(responseData))
+                return
+            }
+            // now we have the post, let's just print it to prove we can access it
+            //print("The post is: " + post.description)
+            
+            // the post object is a dictionary
+            // so we just access the title using the "title" key
+            // so check for a title and print it if we have one
+            if let valid = post["ok"] as? Bool{
+                if(valid){
+                    dispatch_async(dispatch_get_main_queue()) {
+                        self.performSegueWithIdentifier("ResetToLoginSegue", sender: nil)
+                    }
+                }else{
+                    dispatch_async(dispatch_get_main_queue()) {
+                        self.errorLabel.text = post["error"] as? String
+                    }
+                }
+                //self.performSegueWithIdentifier("StartGameSegue", sender: nil)
+            }
+        })
+        task.resume()
+        
+    }
     /*
     // MARK: - Navigation
 
